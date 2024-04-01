8451faefe3a6ad8ae46cb90e31f69b59a02045ea
livinginatent <suleyman.eminbeyli@gmail.com>
2024-04-01 23:38:26 +0400

    fix

--- grocery-shop-challenge/src/App.tsx
+++ grocery-shop-challenge/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,45 +10,84 @@ function App() {
     "Checkout 5",
   ];
 
-  const randomCustomers = () => {
-    const length = Math.floor(Math.random() * 5 + 1);
-    return new Array(length).fill(0).map(() => ({
-      items: randomItems(),
-    }));
-  };
+  const [userInput, setUserInput] = useState<number>(0);
+
+  //TODO: For each checkout, generate an array of random length, fill with random numeric values
+
   const randomItems = () => {
-    return Math.floor(Math.random() * 5 + 1);
+    const length = Math.floor(Math.random() * 5 + 1);
+    return Array.from({ length: length }, () =>
+      Math.floor(Math.random() * 7 + 1)
+    );
   };
 
-  const checkoutData = checkouts.map((checkout) => ({
-    checkout,
-    customers: randomCustomers(),
-  }));
+  const [randomCheckoutItems, setRandomCheckoutItems] = useState(() =>
+    checkouts.map(() => randomItems())
+  );
 
-  const leastItems = () => {
-    let leastItems = Infinity
-    for (const checkout of checkoutData) {
-      const itemSum = checkout.customers.reduce((acc, curr) => {
-        const currentValue = Object.values(curr)[0];
-        return acc + currentValue;
-      }, 0);
-      if(itemSum<leastItems){
-        leastItems=itemSum
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //TODO: Track the sum of items for each corresponding Checkout
+    let leastItems = Infinity;
+    let leastCheckoutIndex = -1;
+    for (let i = 0; i < randomCheckoutItems.length; i++) {
+      const itemSum = randomCheckoutItems[i].reduce(
+        (acc, curr) => acc + curr,
+        0
+      );
+      if (itemSum < leastItems) {
+        leastItems = itemSum;
+        leastCheckoutIndex = i;
       }
     }
-    console.log(leastItems)
+
+    if (leastCheckoutIndex !== -1) {
+      setRandomCheckoutItems((prev) =>
+        prev.map((line, index) => {
+          if (leastCheckoutIndex === index) {
+            return [...line, userInput];
+          } else {
+            return line;
+          }
+        })
+      );
+    }
   };
-  leastItems();
+
+  const onChange = (e) => {
+    setUserInput(e.target.valueAsNumber || 0);
+  };
+
+  //TODO: for each item in each checkout, decrease the values by 1 each 1 second
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRandomCheckoutItems((prevItems) => ([prevItems[0]-1,prevItems.slice(1)]));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+    // Since the effect does not depend on changing values but just sets up an interval, no dependencies are needed here.
+  }, []);
+
   return (
     <>
       <div className="App">
         <div className="input-box">
-          <input placeholder="search" className="input"></input>
-          <button>checkout</button>
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={onChange}
+              type="number"
+              placeholder="search"
+              className="input"
+            ></input>
+            <button>checkout</button>
+          </form>
         </div>
         <div className="store">
           <div className="checkouts">
-            {checkoutData.map(({ checkout, customers }) => (
+            {checkouts.map((checkout, i) => (
               <div className="checkout" key={checkout}>
                 {checkout}
                 <div
@@ -57,11 +97,11 @@ function App() {
                     flexDirection: "column",
                   }}
                 >
-                  {customers.map((customer) => (
-                    <div key={customer.items} className="customer">
-                      {customer.items}
-                    </div>
-                  ))}
+                  <div className="customer">
+                    {randomCheckoutItems[i].map((item, i) => (
+                      <p key={i}>{item}</p>
+                    ))}
+                  </div>
                 </div>
               </div>
             ))}
